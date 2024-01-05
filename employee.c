8002b01a881429b4d1962ad6d07ee958490f77ca
김은범 <icesnake72@gmail.com>
2024-01-06 03:32:54 +0900

    깃 명령 추가

--- employee.c
+++ employee.c
@@ -1,5 +1,9 @@
 #include "employee.h"
 
+// git branch --set-upstream-to=origin/main main
+// git pull --rebase
+// git push -u origin main
+
 int main(int argc, char *argv[]) 
 {
     struct _EMPLOYEE emp;
